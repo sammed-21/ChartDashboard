b3b8e3a02351420c416fba12a2ae12bbab210ee5
sammed-21 <sammed2110@gmail.com>
2023-09-06 10:46:57 +0530

    hide overflow

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,12 @@ import Login from "./login/page";
 import Dashboard from "./dashboard/page";
 // import { SignOut, signIn, getProviders } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+ 
  
 export default function Home() {
   const session = useSession();
   return (
-    <main className=" min-h-screen   items-center justify-between  ">
+    <main className=" min-h-screen overflow-hidden  items-center justify-between  ">
       {!session.data ? <Login /> : <Dashboard />}
     </main>
   );
